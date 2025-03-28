ce663aa8cada64afeede92d22a8c887bf5f55ea1
poudelsachin009@gmail.com <poudelsachin009@gmail.com>
2025-03-28 10:54:37 +0545

    primNum up to n added

---
+++ PrimeNum.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPrime(int num)
+{
+    if (num < 2)
+    {
+        return false;
+    }
+    for (int i = 2; i * i <= num; i++)  
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int num;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    printf("Prime numbers up to %d are:\n", num);
+
+    for (int i = 2; i <= num; i++)
+    {
+        if (isPrime(i))
+        {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}

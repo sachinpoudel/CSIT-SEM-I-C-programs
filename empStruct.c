9a06184d7a536a8885816c3fc871e249d7631b14
poudelsachin009@gmail.com <poudelsachin009@gmail.com>
2025-03-29 15:06:10 +0545

    code added

---
+++ empStruct.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+struct employee {
+    char emp_name[50];
+    int emp_id;
+    float emp_salary;
+};
+
+int main() {
+    struct employee emp1, emp2;
+    struct employee *ptr1 = &emp1;
+    struct employee *ptr2 = &emp2;
+
+    // Input for employee1
+    printf("Enter the details of emp1\n");
+
+    printf("Enter the Name: ");
+    scanf(" %[^\n]", ptr1->emp_name);  // Allows spaces in input
+
+    printf("Enter the ID: ");
+    scanf("%d", &ptr1->emp_id);
+
+    printf("Enter the Salary: ");
+    scanf("%f", &ptr1->emp_salary);
+
+    // Input for employee2
+    printf("\nEnter the details of emp2\n");
+
+    printf("Enter the Name: ");
+    scanf(" %[^\n]", ptr2->emp_name);
+
+    printf("Enter the ID: ");
+    scanf("%d", &ptr2->emp_id);
+
+    printf("Enter the Salary: ");
+    scanf("%f", &ptr2->emp_salary);
+
+    // Displaying details
+    printf("\n%-20s %-10s %-10s\n", "Name", "ID", "Salary");
+    printf("------------------------------------------\n");
+    printf("%-20s %-10d %-10.2f\n", ptr1->emp_name, ptr1->emp_id, ptr1->emp_salary);
+    printf("%-20s %-10d %-10.2f\n", ptr2->emp_name, ptr2->emp_id, ptr2->emp_salary);
+
+    return 0;
+}
